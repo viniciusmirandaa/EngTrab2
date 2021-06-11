71c600bd7fedd49b664d3968daf2d8812846615b
Vinicius Miranda <vini.s.miranda@hotmail.com>
2021-06-11 19:35:31 -0300

    Atualizando conteudo das classes

--- implementacao/src/MercadoOnline/Cliente.java
+++ implementacao/src/MercadoOnline/Cliente.java
@@ -7,6 +7,10 @@ public class Cliente {
     private String endereco;
     private String cpf;
 
+    String []vetCartao;
+    String []auxVetCartao;
+    int i = 0;
+
     public String getNomeCliente() {
         return nomeCliente;
     }
@@ -29,10 +33,23 @@ public class Cliente {
 
     private String infoCartao;
 
-    public void cadastro(){
+    public boolean cadastro(){
         nomeCliente = JOptionPane.showInputDialog(null, "Insira o seu nome: ");
-        endereco = JOptionPane.showInputDialog(null, "Insira o seu endeereco: ");
+        endereco = JOptionPane.showInputDialog(null, "Insira o seu endereço: ");
         cpf = JOptionPane.showInputDialog(null, "Insira o seu CPF: ");
-        infoCartao = JOptionPane.showInputDialog(null, "Insira a informação do seu cartão: ");
+        infoCartao = JOptionPane.showInputDialog(null, "Insira a informação do seu cartão: (ex. 1234 1234 1234)");
+        vetCartao = infoCartao.split(" ");
+        for(String vet : vetCartao){
+            auxVetCartao = vet.split("");
+            if(auxVetCartao.length == 4){
+                auxVetCartao = new String[0];
+                i++;
+            }
+        }
+        if(i == 3){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
--- implementacao/src/MercadoOnline/MainUser.java
+++ implementacao/src/MercadoOnline/MainUser.java
@@ -2,5 +2,14 @@ package MercadoOnline;
 
 public class MainUser {
 
+    public static void main(String[] args) {
+        Cliente usuario = new Cliente();
+        CatalogoCliente catalogoCliente = new CatalogoCliente();
+        boolean result = usuario.cadastro();
+        System.out.println(result);
+        catalogoCliente.mostrarProduto();
+
+    }
+
 
 }
