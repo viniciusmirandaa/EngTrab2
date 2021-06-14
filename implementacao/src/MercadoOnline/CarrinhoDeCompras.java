d0a888edb768b38a6035e5b642ab3d6ad4244104
Vinicius Miranda <vini.s.miranda@hotmail.com>
2021-06-13 21:05:15 -0300

    Adicionando método de análise

--- implementacao/src/MercadoOnline/CarrinhoDeCompras.java
+++ implementacao/src/MercadoOnline/CarrinhoDeCompras.java
@@ -5,21 +5,47 @@ import java.util.HashMap;
 
 public class CarrinhoDeCompras {
     public HashMap<Produto, Integer> produtosCarrinho = new HashMap<>();
+    public HashMap<Produto, Integer> auxAnalise = new HashMap<>();
     public static HashMap<Produto, Integer> produtosAnalise = new HashMap<>();
     private static double totalCompra;
+    String mostrarCarrinho = "";
+    int vendeuMais = 1;
+    int vendeuMenos = 1;
 
-    public void analiseProdutos(){
-        for(Produto produto : produtosAnalise.keySet()){
-//            if()
+    public void analiseProdutos(Produto.Categoria categoria) {
+        for (Produto produto : produtosAnalise.keySet()) {
+            if (produto.getCategoria().equals(categoria)) {
+                auxAnalise.put(produto, produtosAnalise.get(produto));
+            }
+        }
+
+        for (Produto produto : auxAnalise.keySet()) {
+            if (auxAnalise.get(produto) > vendeuMais)
+                vendeuMais = auxAnalise.get(produto);
+            else if (auxAnalise.get(produto) < vendeuMais)
+                vendeuMenos = auxAnalise.get(produto);
+        }
+
+        for (Produto produto : auxAnalise.keySet()) {
+            if (auxAnalise.get(produto) == vendeuMais) {
+                JOptionPane.showMessageDialog(null, "O produto " + produto.getNomeProduto() + " vendeu mais na categoria "
+                        + produto.getCategoria());
+                auxAnalise = new HashMap<>();
+            } else if (auxAnalise.get(produto) == vendeuMenos) {
+                JOptionPane.showMessageDialog(null, "O produto " + produto.getNomeProduto() + " vendeu menos na categoria "
+                        + produto.getCategoria());
+                auxAnalise = new HashMap<>();
+            }
         }
     }
 
     public boolean adicionarProdutos(Produto p, int quantidade) {
         if (produtosCarrinho.containsKey(p)) {
             produtosCarrinho.replace(p, produtosCarrinho.get(p) + quantidade);
-            produtosAnalise.put(p, quantidade);
+            produtosAnalise.replace(p, produtosAnalise.get(p) + quantidade);
             return true;
         } else {
+            produtosAnalise.put(p, quantidade);
             produtosCarrinho.put(p, quantidade);
             return true;
         }
@@ -38,9 +64,11 @@ public class CarrinhoDeCompras {
 
     public void mostrarProdutosCarrinho() {
         for (Produto p : produtosCarrinho.keySet()) {
-            JOptionPane.showMessageDialog(null, "Produto: " + p.getNomeProduto() + "\n"
-                    + "Preço: " + p.getPrecoProduto() + "\n" + "Quantidade: " + produtosCarrinho.get(p));
+            String preco = String.valueOf(p.getPrecoProduto());
+            mostrarCarrinho += p.getNomeProduto() + "  Preço: " + preco + "  Quantidade: " + produtosCarrinho.get(p) + "\n";
         }
+        JOptionPane.showMessageDialog(null, mostrarCarrinho);
+        mostrarCarrinho = "";
     }
 
     public double valorTotalCarrinho() {
